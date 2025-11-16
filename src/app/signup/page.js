3b6a583b12ee54226412d569e83640a241866ec6
Rituraj Rai <162144998+riturajrai@users.noreply.github.com>
2025-11-16 15:25:34 +0530

    Update page.js

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -41,7 +41,7 @@ export default function SignupPage() {
   const onSubmit = async (data) => {
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/signup", {
+      const res = await fetch("https://qrreviewbackend.onrender.com/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
